a7280bc7282a7d2856d8e25f483375111056bd10
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-08-15 16:24:07 +0100

    refactoring

--- js/server.js
+++ js/server.js
@@ -15,58 +15,61 @@ const server = http.createServer((req, res) => {
 
   function flipCheck(flipRequest, flipResult) {
     return flipRequest === flipResult
-      ? `${flipResult} You Win!`
-      : `${flipResult} You Lose!`;
+      ? `It's ${flipResult} You Win!`
+      : `It's ${flipResult} You Lose!`;
   }
 
-  // Option to load the basic index.html file - working
-  if (page == "/") {
-    fs.readFile("./../index.html", "utf-8", function (err, data) {
+  function readWrite(file, contentType) {
+    fs.readFile(file, function (err, data) {
       if (err) {
         console.log(data, typeof data);
       } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writeHead(200, { "Content-Type": contentType });
         res.write(data);
         res.end();
       }
     });
   }
-  // Option to handle the button push server request to flip a coin
-  else if (page == "/api") {
-    const flipR = params["flipRequest"];
-    if ("flipRequest" in params) {
-      let flipResult = Math.random() <= 0.5 ? "heads" : "tails";
-      let result = flipCheck(flipR, flipResult);
-      let message = flipResult === "heads" ? "img/heads.png" : "img/tails.png";
 
-      if (params["flipRequest"]) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
-          result: result,
-          message: message,
-        };
-        res.end(JSON.stringify(objToJson));
+  switch (page) {
+    // Option to load the basic index.html file - working
+    case "/":
+      readWrite("./../index.html", "text/html");
+      break;
+    // Option to handle the button push server request to flip a coin
+    case "/api":
+      const flipR = params["flipRequest"];
+      if ("flipRequest" in params) {
+        let flipResult = Math.random() <= 0.5 ? "heads" : "tails";
+        let result = flipCheck(flipR, flipResult);
+        let message =
+          flipResult === "heads" ? "img/heads.png" : "img/tails.png";
+
+        if (params["flipRequest"]) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          const objToJson = {
+            result: result,
+            message: message,
+          };
+          res.end(JSON.stringify(objToJson));
+        }
       }
-    }
-  }
-  // Option to load the basic css file - working
-  else if (page == "/css/style.css") {
-    fs.readFile("./../css/style.css", function (err, data) {
-      res.write(data);
-      res.end();
-    });
+      break;
+    // Option to load the basic css file - working
+    case "/css/style.css":
+      fs.readFile("./../css/style.css", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
     // Option to load the basic js file - working
-  } else if (page == "/js/main.js") {
-    fs.readFile("./../js/main.js", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-      res.write(data);
-      res.end();
-    });
-  } else {
+    case "/js/main.js":
+      readWrite("./../js/main.js", "text/javascript");
+      break;
     // Throw an error if an invalid request file
-
-    console.log("Something went wrong...");
-    return;
+    default:
+      console.log("Something went wrong...");
+      return;
   }
 });
 
